b57dced14a71ce2399f306b87ae5700716743774
MrDave <davidschnoll@gmail.com>
2023-08-30 23:24:25 +0300

    Return errors when encountered

--- scripts.py
+++ scripts.py
@@ -1,4 +1,5 @@
 from datacenter.models import Schoolkid, Mark, Chastisement, Subject, Lesson, Commendation
+from random import choice
 
 
 def fix_marks(kid: str):
@@ -16,25 +17,46 @@ def remove_chastisements(kid: str):
 
 def create_commendation(kid: str, subject_name: str):
     schoolkid = fetch_kid(kid)
-    subject = Subject.objects.get(
-        title=subject_name,
-        year_of_study=schoolkid.year_of_study
-    )
-    lesson = Lesson.objects.filter(
-        year_of_study=schoolkid.year_of_study,
-        group_letter=schoolkid.group_letter,
-        subject=subject
-    ).order_by("-date").first()
+    try:
+        subject = Subject.objects.get(
+            title=subject_name,
+            year_of_study=schoolkid.year_of_study
+        )
 
-    Commendation.objects.create(
-        text="Молодцом!",
-        created=lesson.date,
-        schoolkid=schoolkid,
-        subject=subject,
-        teacher=lesson.teacher,
-    )
+        lesson = Lesson.objects.filter(
+            year_of_study=schoolkid.year_of_study,
+            group_letter=schoolkid.group_letter,
+            subject=subject
+        ).order_by("-date").first()
+
+        text_choices = [
+            "Молодцом!",
+            "Хвалю!",
+            "Внимательно слушает учителя",
+            "Лучший ответ в классе!",
+            "Так держать!",
+            "Невероятный успех!",
+            "Лучше всех!",
+            "Пример для подражания",
+            "Гордость класса!",
+        ]
+
+        Commendation.objects.create(
+            text=choice(text_choices),
+            created=lesson.date,
+            schoolkid=schoolkid,
+            subject=subject,
+            teacher=lesson.teacher,
+        )
+    except Subject.DoesNotExist:
+        print("Нет такого предмета")
 
 
 def fetch_kid(kid_name: str):
-    school_kid = Schoolkid.objects.get(full_name__contains=kid_name)
-    return school_kid
+    try:
+        kid = Schoolkid.objects.get(full_name__contains=kid_name)
+        return kid
+    except Schoolkid.DoesNotExist:
+        print("Такого имени не существует")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Найдено больше 1 совпадения")
